d43aa6ffc17a45e1ad54467aa3c1daca96d108b5
juG90 <Julia.Geigl@gmx.de>
2016-11-24 19:03:32 +0100

    Möglich Daten an adafruit zu senden

--- SampleCode.ino
+++ SampleCode.ino
@@ -7,16 +7,19 @@ Rx3 - ESP8266 Tx
 pin 53 - ESP8266 CH_PD (Always High)
 */
 
+enum WaitFor { OK ,SEND_OK, READY };
 
 #define ESP8266 Serial3
-String SSID = "test";
-String PASSWORD = "test1234";
+String SSID = "xxx";
+String PASSWORD = "xxx";
+String IpAddress = "xxx"; // IP Addresse muss im eigenen Subnetz liegen
 
 
 #define SERVER "www.google.com"
 #define PORT 80
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883  
+
 
 //always high
 int CH_PD_8266 = 53;
@@ -34,100 +37,38 @@ void setup() {
   
     //Wait Serial Monitor to start
     while(!Serial);
-    Serial.println("--- Start ---");
+    Serial.println("- Start -");
 
     ESP8266.println("AT+RST");
     delay(1000);
 
     ESP8266.end();
     ESP8266.begin(115200);
-    Serial.println("--- Restart ---");
+    Serial.println("- Restart -");
     
     Serial.println("Module is ready\n AT+CWMODE=1");
     ESP8266.println("AT+CWMODE=1");
+    waitfromESP8266(10, OK);
     ESP8266.println("AT+CWMODE?");
 
-    delay(2000);
+    waitfromESP8266(10, OK);
       
     //Quit existing AP, for demo
       ESP8266.println("AT+CWQAP");
-      Serial.println("=======Disconnect from AP: AT+CWQAP =========");
-      delay(1000);
-
-     
+      Serial.println("= Disconnect from AP: AT+CWQAP =");
+      waitfromESP8266(10, OK);
+      
       clearESP8266SerialBuffer();
-      delay(1000);
 
-
-      Serial.println("======= AT+CIPMUX=1 =========");
+      Serial.println("= AT+CIPMUX=1 =");
       ESP8266.println("AT+CIPMUX=1");
-      delay(1000);
-      
+      waitfromESP8266(10, OK);
+     
       cwJoinAP();
-      delay(1000);
+      waitfromESP8266(10, OK);
       cwGetIP();
-      delay(1000);
 
-      // connect to google
-      //String cmd= "AT+CIPSTART=4,\"TCP\",\"" + SERVER +"\"" + PORT;
-      delay(1000);
-    //  String cmd= "AT+CIPSTART=4,\"TCP\",\"52.70.203.194\",80"; //adafruit
-    //  String cmd= "AT+CIPSTART=4,\"TCP\",\"216.58.209.35\",80"; //Google
-     String cmd= "AT+CIPSTART=4,\"TCP\",\"52.70.203.194\",80"; //adafruit
-         
-      Serial.println("\n======= "+cmd);
-      ESP8266.println(cmd);
-
-      delay(10000);
-           
-      // Test Nachricht
-      Serial.println("======= startString,");
-      //String headerGET = "GET /api/feeds?x-aio-key=129f2e69ceed4cc0a71012e5cd1e9202 HTTP/1.1\r\n"; // FeedListe
-      //String headerGET = "GET /api/feeds/twar?x-aio-key=b4ef461011dd40a5ba22e39c36d6de87 HTTP/1.1\n"; // DHT Wert abfragen
-      
-     String headerGET = "GET /api/groups/weather/send.json?x-aio-key=f27fa137d3934140a44974f37003de00&testfeed=1 HTTP/1.1\r\n";
-    headerGET  += "Host: io.adafruit.com\r\n\r\n";
-    Serial.println(headerGET);
-   // headerGET  += "Connection: close\n";
-   // headerGET  += "Accept-Charset: ISO-8859-1,UTF-8;q=0.7,*;q=0.7\n ";
-   // headerGET  += "Cache-Control: no-cache \n ";
-   // headerGET  += "Accept-Language: de,en;q=0.7,en-us;q=0.3\n";
-   // headerGET  += "Referer: http://web-sniffer.net/\n\n";
-      
-      int headerGetLen = headerGET.length() + 10;
-      Serial.println(headerGetLen);
-      Serial.println("======= AT+CIPSEND=4,"+headerGetLen);
-      ESP8266.println("AT+CIPSEND=4,"+headerGetLen);
-      ESP8266.println(headerGET);
-
-      delay(1000);
-
-
-
-      
-//
-//    String headerPOST ="POST /api/feeds/welcome-feed/data/send HTTP/1.1\n";
-//    headerPOST  += "Host: io.adafruit.com\n";
-//    headerPOST  += "Connection: close\n";
-//    headerPOST  += "Accept-Charset: ISO-8859-1,UTF-8;q=0.7,*;q=0.7\n ";
-//    headerPOST  += "Cache-Control: no-cache \n ";
-//    headerPOST  += "Accept-Language: de,en;q=0.7,en-us;q=0.3\n";
-//    headerPOST  += "Referer: http://web-sniffer.net/\n";
-//    headerPOST  += "Content-type: application/x-www-form-urlencoded\n";
-//
-//    String msgPOST = "x-aio-key=129f2e69ceed4cc0a71012e5cd1e9202\n";
-//    msgPOST += " {\"feedID\": \"welcome-feed\", \"data\": {\"value\": 586}}";
-//
-//     int msgPostLen = msgPOST.length();
-//    headerPOST  += ("Content-length: %d\n\n",msgPostLen);
-//    
-//    int headerMsgPostLen = msgPostLen + headerPOST.length();
-//   // Serial.println("======= AT+CIPSEND=4,"+headerMsgPostLen);
-//      //ESP8266.println("AT+CIPSEND=4,"+headerMsgPostLen);
-//      //ESP8266.println(headerPOST);
-//  //ESP8266.println(msgPOST);
-//  //}while(FAIL_8266);
-// 
+     sendValue("test-feed",56);
 }
 
 void loop() {
@@ -136,6 +77,56 @@ void loop() {
     Serial3.write(a);
   }
 }
+
+bool sendValue (String feed, int value)
+{
+  String cmd= "AT+CIPSTART=4,\"TCP\",\"io.adafruit.com\",80";
+         
+  Serial.println("= "+cmd);
+  ESP8266.println(cmd);
+
+  delay(500);
+           
+  // Test Nachricht
+  Serial.println("= startString,");
+  //String headerGET = "GET /api/feeds?x-aio-key=129f2e69ceed4cc0a71012e5cd1e9202 HTTP/1.1\r\n"; // FeedListe
+  //String headerGET = "GET /api/feeds/twar?x-aio-key=b4ef461011dd40a5ba22e39c36d6de87 HTTP/1.1\n"; // DHT Wert abfragen
+
+   //GET /api/groups/weather/send.json?x-aio-key=f27fa137d3934140a44974f37003de00&testfeed=1 HTTP/1.1\r\n
+  String headerGET = "GET /api/groups/weather/send.json?x-aio-key=129f2e69ceed4cc0a71012e5cd1e9202&";
+  headerGET += feed;
+  headerGET += "=";
+  headerGET += value;
+  headerGET += " HTTP/1.1\r\n";
+  headerGET  += "Host: io.adafruit.com\r\n\r\n";
+      
+  int headerGetLen = headerGET.length();
+  //Serial.println(headerGET);
+  // Serial.println("= AT+CIPSEND=4,"+headerGetLen);
+      
+  // if (waitOKfromESP8266(1000))
+  ESP8266.print("AT+CIPSEND=4,");
+  ESP8266.println(headerGetLen);
+
+      
+  if (waitfromESP8266(100, OK))
+  {
+    //Serial.println("send");
+    ESP8266.print(headerGET);
+    ESP8266.print("+++");
+    if(waitfromESP8266(10, SEND_OK))
+    {
+      Serial.println("true");
+      return true;
+    }
+    else
+    {
+      Serial.println("false");
+      return false;
+    }
+  }
+}
+
 
 void serialEvent3() {
   while (ESP8266.available() > 0) {
@@ -147,8 +138,13 @@ void serialEvent3() {
 
 void cwJoinAP()
 {
-  String cmd="AT+CWJAP=\"" + SSID + "\",\"" + PASSWORD + "\"";
-  Serial.println("============ Connect to =========");
+  String cmd ="AT+CIPSTA=\"" + IpAddress + "\"";
+      ESP8266.println(cmd);
+      delay(1000);
+
+      
+   cmd="AT+CWJAP=\"" + SSID + "\",\"" + PASSWORD + "\"";
+  Serial.println("= Connect to =");
   Serial.println(cmd);
   ESP8266.println(cmd);
   delay(5000);
@@ -161,13 +157,14 @@ void cwGetIP()
   delay(1000);
   //clearESP8266SerialBuffer();
   ESP8266.println("AT+CIFSR");
-  Serial.println("======= GET IP:  AT+CIFSR ========");
+  Serial.println("= GET IP:  AT+CIFSR =");
   delay(1000);
   
   while (ESP8266.available() > 0) {
     char a = ESP8266.read();
     Serial.write(a);
   }
+  delay(500);
 }
 
 void clearESP8266SerialBuffer()
@@ -177,6 +174,45 @@ void clearESP8266SerialBuffer()
           char a = ESP8266.read();
            Serial.write(a);
         }
-      Serial.println("======== Clear Buffer End ==============");
+      Serial.println("= Clear Buffer End =");
       delay(1000);
 }
+boolean waitfromESP8266(int timeout, int type)
+{
+  do{
+    Serial.println("= wait .. ");
+    delay(50);
+    switch (type)
+    {
+      case OK:
+      {
+        if(ESP8266.find("OK"))
+        {
+          return true;
+        }
+        break;
+      }
+      case SEND_OK:
+      {
+        if(ESP8266.find("SEND OK"))
+        {
+          return true;
+        }
+        break;
+      }
+      case READY:
+      {
+        if(ESP8266.find("ready"))
+        {
+          return true;
+        }
+        break;
+      }
+    }
+    //delay(50);
+  }while((timeout--)>0);
+  return false;
+}
+
+
+
