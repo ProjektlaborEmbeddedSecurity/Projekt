9256746490ff252f589a48b7f2bc056482a7c5a2
claybear <1631584@stud.hs-mannheim.de>
2016-12-01 10:42:30 +0100

    Enum count

--- SampleCode.ino
+++ SampleCode.ino
@@ -37,7 +37,7 @@ SimpleDHT11 dht11;
 enum WaitFor { OK ,SEND_OK, READY };
 
 
-
+//	      0	       1	  2	     3			4		   5		       6		   7		8	     9		10	   11		      12		 13		     14			 15	      16
 enum Zustand {AllesOk, FeOZuWarm, FeOZuKalt, FeOZuWarmZuFeucht, FeOZuKaltZuFeucht, FeOZuWarmZuTrocken, FeOZuKaltZuTrocken, FeOZuFeucht, FeOZuTrocken,FeZZuWarm, FeZZuKalt, FeZZuWarmZuFeucht, FeZZuKaltZuFeucht, FeZZuWarmZuTrocken, FeZZuKaltZuTrocken, FeZZuFeucht, FeZZuTrocken };
 
 
